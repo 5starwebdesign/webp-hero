21f8a1f1f40e49b3eea024c5950283cf65592bed
Chase Moskal <chasemoskal@gmail.com>
2017-12-18 06:08:42 -0800

    deploy

---
+++ dist/interfaces.d.ts
@@ -0,0 +1,8 @@
+import { Webp } from "../libwebp/dist/webp";
+export interface WebpMachineOptions {
+    webp?: Webp;
+    webpSupport?: Promise<boolean>;
+}
+export interface PolyfillDocumentOptions {
+    document?: Document;
+}
